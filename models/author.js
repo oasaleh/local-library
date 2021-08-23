59d8992aaf19601b2bc641d19e6c42b639035e68
Omar <omarasaleh@hotmail.com>
2021-08-23 16:42:38 -0500

    Create author view, add list functino in author controller

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const { Schema } = mongoose;
 
@@ -36,5 +37,15 @@ AuthorSchema.virtual('url').get(function () {
   return '/catalog/author/' + this._id;
 });
 
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : 'Unknown';
+});
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : 'Unknown';
+});
 // Export model
 module.exports = mongoose.model('Author', AuthorSchema);
