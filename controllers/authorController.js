06a2b6aa6e155c777f9c9b9c886d5cb1d4e0e4f0
Omar <omarasaleh@hotmail.com>
2021-08-23 21:57:51 -0500

    Create author controller func and view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,7 @@
 // Import model
+const async = require('async');
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 // Display list of all Authors.
 exports.author_list = function (req, res, next) {
@@ -18,8 +20,35 @@ exports.author_list = function (req, res, next) {
 };
 
 // Display detail page for a specific Author.
-exports.author_detail = function (req, res) {
-  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+exports.author_detail = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.params.id }, 'title summary').exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        // No results.
+        var err = new Error('Book not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      console.log(results);
+      res.render('author_details', {
+        title: 'Author Detail',
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    },
+  );
 };
 
 // Display Author create form on GET.
